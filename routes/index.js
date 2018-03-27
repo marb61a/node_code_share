ba6ba0c3d94682b6fb1112e5ec561e8755af4b2f
marb61a <marb61a@gmail.com>
2018-03-27 12:40:26 +0100

    updated views and routes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Martin' });
+  res.render('index', { title: 'Code-Share : A platform for sharing your code and coding'});
 });
+
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'Code-Share : A platform for sharing your code and coding'});     									
+});
+
+router.route('/contact')
+	.get(function(req, res, next) {
+	  res.render('contact', { title: 'Code-Share : A platform for sharing your code and coding'});     									
+	})
+	.post(function(req, res, next){
+		res.render('thank', { title: 'Code-Share : A platform for sharing your code and coding'});
+	})
 
 module.exports = router;

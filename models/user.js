1545cdcb2667ca1d3fc79c112509d1e95812b253
marb61a <marb61a@gmail.com>
2018-04-03 13:12:27 +0100

    added user model and updated style.css

---
+++ models/user.js
@@ -0,0 +1,18 @@
+var mongoose = require('mongoose');
+var crypto = require('crypto');
+
+var userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  hash: String,
+  salt: String
+});
+
+module.exports = mongoose.model('User', userSchema);

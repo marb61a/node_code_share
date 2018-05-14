48ffaf828b6bf63abed79aed2c7a8160ed1d5e7a
marb61a <marb61a@gmail.com>
2018-05-14 12:12:08 +0100

    added task view file and updated auth routes file

--- routes/auth.js
+++ routes/auth.js
@@ -6,5 +6,10 @@ router.route('/login')
 	.get(function(req, res, next){
 		res.render('login', { title: 'Login your account'});
 	})
+	.post(passport.authenticate('login' {
+    	failureRedirect: '/login'
+  	}), function(req, res){
+		res.redirect('/');
+	})
 
 module.exports = router;

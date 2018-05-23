fac60626bee9f1ffcfd3614373ffcabfa0270ad4
marb61a <marb61a@gmail.com>
2018-05-23 17:10:48 +0100

    updated layout and user model

--- models/user.js
+++ models/user.js
@@ -16,4 +16,14 @@ var userSchema = new mongoose.Schema({
   facebookId: String
 });
 
+userSchema.methods.setPassword = function(password){
+  this.salt = crypto.randomBytes(16).toString('hex');
+  this.hash = crypto.pbkdf2sync(password, this.salt, 1000, 64, 'sha1').toString('hex');
+};
+
+userSchema.methods.validPassword = function(password){
+  this.hash = crypto.pbkdf2sync(password, this.salt, 1000, 64, 'sha1').toString('hex');
+  return this.hash === hash;
+};
+
 module.exports = mongoose.model('User', userSchema);
